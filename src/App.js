c7d98683a313bf73de7f4dfe4da9e5dcc85bc89d
Yago <yagoponcem@gmail.com>
2022-11-01 18:38:34 +0100

    done 1 2 3 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import contactsArr from "./contacts"
 function App() {
+                          //iteración 1
+  const contacts = contactsArr.slice(0,5)
+  const [personList, setPersonList] = useState(contacts)
+
+                           //iteracion 3
+                         //intento de hacer un map dentro de un filter. no sale.
+// const randomPerson = () => {
+// const filteredPerson = contactsArr.filter((eachPerson) => {
+//   if (eachPerson.id === personList.map((eachId) => eachId.id)) {
+//     return true
+//   } else {
+//     return false
+//   }
+// })
+// const randomNum = Math.floor(Math.random() * filteredPerson.length)
+// return setPersonList(filteredPerson[randomNum])
+// }
+
+const randomPerson = () => {
+  const copyPersonList = personList.map((eachElement) => eachElement)
+  const randomNum = Math.floor(Math.random() * contactsArr.length)
+  const newContact = contactsArr[randomNum]
+  copyPersonList.push(newContact)
+  return setPersonList(copyPersonList)
+}
+
+
+                            //iteración 4
+  const orderedByPopularity = () => {
+    const copyPersonList = personList.map((eachElement) => eachElement)
+    copyPersonList.sort((elem1, elem2) => {
+      if  (elem1.popularity > elem2.popularity) {
+        return -1
+    } else {
+        return 1
+    }
+    })
+    setPersonList(copyPersonList)
+
+  }
+
+  const orderedByName = () => {
+    const copyPersonList = personList.map((eachElement) => eachElement)
+    copyPersonList.sort((elem1, elem2) => {
+      if  (elem1.name > elem2.name) {
+        return 1
+    } else {
+        return -1
+    }
+    })
+    setPersonList(copyPersonList)
+  }
+
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+    <button onClick={randomPerson}>Add Random Contact</button>
+    <button onClick={orderedByPopularity}>Sort by popularity </button>
+    <button onClick={orderedByName}>Sort by name</button>
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        </thead>
+{personList.map((eachPerson) => {
+    return (
+      <tbody>
+      <tr> 
+      <td><img src={eachPerson.pictureUrl} atl="celebrity" class="actorImg"/></td>
+      <td>{eachPerson.name}</td>
+      <td>{eachPerson.popularity.toFixed(2)}</td>
+      <td>{eachPerson.wonOscar === true && "🏆"}</td>
+      <td>{eachPerson.wonEmmy === true && "🏆"}</td>
+    </tr>
+    </tbody>
+
+   
+      )
+    })}
+
+      </table>
+
+
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
